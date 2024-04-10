242ded54e16405280bbafaddc03a6628db3de0f0
janie <ioana.ileana@gmail.com>
2024-04-10 19:26:59 +0200

    started heapsorts

--- include/DataBase.hpp
+++ include/DataBase.hpp
@@ -11,6 +11,14 @@ class DataBase {
 private:
       /**< The vector of data points. */
       std::vector<DataPoint> _data_points;
+      
+      //indices of points sorted by category then TS
+      int* sortedIndByCatAndTS;
+      
+      //indices of point sorted solely by TS
+      int* sortedIndByTS;
+      
+      int _countPoints;
 
 public:
      /**
@@ -18,15 +26,23 @@ public:
      * @param data_points The vector of data points.
      */
      DataBase(const char* filename);
+      /**
+     * @brief Destroys the DataBase object.
+     */
+    ~DataBase();
+    
+  
      /**
      * @brief Gets the vector of data points.
      * @return The vector of data points.
      */
      std::vector<DataPoint>& GetPoints();
+     
      /**
      * @brief Gets a data point from the database at the specified index.
      * @param index The index of the data point to get.
      * @return The data point at the specified index.
+     
      */
      DataPoint& GetPoint(int index);
      /**
@@ -34,10 +50,13 @@ public:
      * @return The size of the database.
      */
      int GetSize();
-     /**
-     * @brief Destroys the DataBase object.
-     */
-    ~DataBase();
+     
+     void SortByCatAndTS();
+
+private:
+     
+     void SiftByCatAndTS(int i, int n);
+
 };
 
 #endif 
--- src/DataBase.cpp
+++ src/DataBase.cpp
@@ -16,6 +16,10 @@ DataBase::DataBase(const char* filename) {
           DataPoint p(ifs);
           _data_points.push_back(p);
      }
+     _countPoints = N;
+}
+
+DataBase::~DataBase() {
 }
 
 std::vector<DataPoint>& DataBase::GetPoints(){
@@ -28,9 +32,23 @@ DataPoint& DataBase::GetPoint(int index) {
 }
 
 int DataBase::GetSize() {
-    return _data_points.size();
+    return _countPoints;
+}
+
+void DataBase::SiftByCatAndTS(int i, int n){
+}
+
+void DataBase::SortByCatAndTS() {
+     sortedIndByCatAndTS = new int[_countPoints];
+     for (int i = 0; i<_countPoints; ++i)
+          sortedIndByCatAndTS[i] = i;
+     
+     for (int i =_countPoints/2; i>=0; i--)
+          SiftByCatAndTS(i,_countPoints);
+     
+     for (int i =_countPoints/2; i>=0; i--)
+          SiftByCatAndTS(i,_countPoints);
+     
 }
 
 
-DataBase::~DataBase() {
-}
