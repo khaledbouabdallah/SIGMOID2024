fbf5ef59a8d8852e2c6fe6b1b1bcc07f5fc447b6
janie <ioana.ileana@gmail.com>
2024-04-11 19:31:40 +0200

    distance function

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -5,4 +5,6 @@ void swapIndices(int* indicesArray, int ind1, int ind2);
 
 float calculateMean(float* data, int size);
 
+float getDistance(float* data1, float* data2);
+
 #endif
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,4 +1,6 @@
 #include "Utils.hpp"
+#include "globals.hpp"
+#include <cmath>
 
 void swapIndices(int* indicesArray, int ind1, int ind2) {
      int aux = indicesArray[ind1]; 
@@ -13,4 +15,11 @@ float calculateMean(float* data, int size) {
           sum += data[i];
      }
      return sum / size;
+}
+
+float distance(float* data1, float* data2) {
+     float sum = 0;
+     for (int i = 0; i < DATA_SIZE; i++) 
+          sum += data1[i] * data2[i];
+     return sqrt(sum);
 }
