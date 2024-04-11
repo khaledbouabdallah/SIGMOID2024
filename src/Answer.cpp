a26eac07cb59855ff303576110385b332ecf9d89
Jb Hochet <jean-baptiste.hochet@proton.me>
2024-04-11 23:39:32 +0200

    implement SiftUp and SiftDown in Answer

--- src/Answer.cpp
+++ src/Answer.cpp
@@ -7,23 +7,52 @@ Answer::~Answer(){
 }
 
 void Answer::CheckAndAdd(int indice, float distance) {
-     if (_countPoints < DATA_SIZE) {
-          _nns[_countPoints]._indice = indice;
-          _nns[_countPoints]._distance = distance;
-          if (_countPoints)
-               SiftUp(_countPoints);
-          _countPoints++;
-     } else if (distance<_nns[0]._distance) {
-          _nns[0]._indice = indice;
-          _nns[0]._distance = distance;
-          SiftDown(0);
-     }
+   if (_countPoints < DATA_SIZE) {
+      _nns[_countPoints]._indice = indice;
+      _nns[_countPoints]._distance = distance;
+      if (_countPoints)
+         SiftUp(_countPoints);
+      _countPoints++;
+   } else if (distance<_nns[0]._distance) {
+      _nns[0]._indice = indice;
+      _nns[0]._distance = distance;
+      SiftDown(0);
+   }
 }
 
 void Answer::SiftUp(int indice) {
-     // a toi de jouer Jb : p
+   while(1) {
+      int indmin = indice;
+      // check
+      if(_nns[indice/2]._distance < _nns[indice]._distance)
+         indmin = indice/2;
+      if(indmin == indice)
+         break;
+      // swap values
+      Neighbour temp_neighbour = _nns[indmin];
+      _nns[indmin] = _nns[indice];
+      _nns[indice] = temp_neighbour;
+      // update the indice
+      indice = indmin;
+   }
 }
 
 void Answer::SiftDown(int indice) {
-     // a toi de jouer Jb : p
+   int n = _countPoints + 1;
+   while(1) {
+      int indmax = indice;
+      // check
+      if(2*indice+1<n && (_nns[2*indice+1]._distance > _nns[indmax]._distance))
+         indmax = 2*indice+1;
+      if(2*indice+2<n && (_nns[2*indice+2]._distance > _nns[indmax]._distance))
+         indmax = 2*indice+2;
+      if(indmax == indice)
+         break;
+      // swap values
+      Neighbour temp_neighbour = _nns[indmax];
+      _nns[indmax] = _nns[indice];
+      _nns[indice] = temp_neighbour;
+      // update the indice
+      indice = indmax;
+   }
 }
