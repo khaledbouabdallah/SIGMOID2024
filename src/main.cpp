3aed9efdd19afcf88703547185d0caad8e84cace
janie <ioana.ileana@gmail.com>
2024-04-23 10:21:34 +0200

    range no threads

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ int main() {
      //const char* queriesInput = "../data/Public-4M-queries.bin";
      
      int runType = 0; //0 = normal, 1 = multi-thread
-     int queryType = 0; //0 = seq scan, 1 = seq scan range, 2 = seq scan incremental, 3 = seq scan range incremental, 4 = sax filter range
+     int queryType = 1; //0 = seq scan, 1 = seq scan range, 2 = seq scan incremental, 3 = seq scan range incremental, 4 = sax filter range
      
      const char* ansoutput = "output.bin";
      //const char* ansoutput = "../data/dummy-output-current.bin";
