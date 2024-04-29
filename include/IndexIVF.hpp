2c2b466dee4321d00b0b77a5f2723d96c0b6a437
Bouabdallah Khaled <bouabdallah.khaled@yahoo.com>
2024-04-29 20:37:09 +0200

    idk

--- include/IndexIVF.hpp
+++ include/IndexIVF.hpp
@@ -10,7 +10,4 @@ public:
     ~IndexIVF();  
 };
 
-
-
-
 #endif
---
+++ src/IndexIVF.cpp
@@ -0,0 +1,14 @@
+#include "../include/IndexIVF.hpp"
+#include "../include/DataBase.hpp"
+#include "../faiss/IndexFlat.h"
+
+
+
+
+IndexIVF::IndexIVF(const DataBase& db): {
+
+    faiss::IndexFlatL2 index();
+
+}
+    
+
