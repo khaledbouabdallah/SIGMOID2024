89efe54720c253774bcacfbce8427f2fbad41c1c
MohamedBouanane <jm_bouanane@esi.dz>
2024-05-01 12:09:26 +0200

    test compilation

--- tests/kmeans_test.cpp
+++ tests/kmeans_test.cpp
@@ -9,14 +9,14 @@ volatile int done = 0;
 int main() {
     // Your code here
 
-    // float a[10] = {-1,-1,0,0,0,6,7,8,9,10};
-    // float b[10] = {1,2,3,4,5,6,7,8,9,10};
+     float a[10] = {-1,-1,0,0,0,6,7,8,9,10};
+     float b[10] = {1,2,3,4,5,6,7,8,9,10};
 
-    float *a; float *b;
 
 
     //float distance = getDistance(a, b);
     float distance = distance_squared(a, b);
+    printf("%f",distance);
 
 
 
