844a76e77a4aebefc1121216cb78618c3825f219
Bouabdallah Khaled <bouabdallah.khaled@yahoo.com>
2024-05-01 11:52:05 +0200

    c++ include sucks

---
+++ tests/kmeans_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <array>
+#include "../include/Kmeans.hpp"
+//#include "Utils.hpp"
+
+
+volatile int done = 0;
+
+int main() {
+    // Your code here
+
+    // float a[10] = {-1,-1,0,0,0,6,7,8,9,10};
+    // float b[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    float *a; float *b;
+
+
+    //float distance = getDistance(a, b);
+    float distance = distance_squared(a, b);
+
+
+
+
+
+
+    
+
+    return 0;
+}
