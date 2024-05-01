3a8eda400d96f57a29ddb53d409b21c0cfe10f8c
Bouabdallah Khaled <bouabdallah.khaled@yahoo.com>
2024-05-01 15:04:39 +0200

    other tests

--- tests/kmeans_test.cpp
+++ tests/kmeans_test.cpp
@@ -5,33 +5,8 @@
 //#include "Utils.hpp"
 
 
-void runTests() {
-    // Test case 1: Empty means and data vectors
-    // std::vector<float*> emptyMeans;
-    // std::vector<float*> emptyData;
-    // std::vector<float> result1 = closest_distance(emptyMeans, emptyData);
-    // assert(result1.empty());
-
-    // // Test case 2: Single mean and single data point
-    // float mean1[2] = {1.0, 2.0};
-    // float data1[2] = {3.0, 4.0};
-    // std::vector<float*> means2 = {mean1};
-    // std::vector<float*> data2 = {data1};
-    // std::vector<float> result2 = closest_distance(means2, data2);
-    // assert(result2.size() == 1);
-    // assert(result2[0] == 5.0);
-
-    // Test case 3: Multiple means and data points
-    // float mean2[2] = {0.0, 0.0};
-    // float data2[2] = {1.0, 1.0};
-    // float data3[2] = {-1.0, -1.0};
-    // std::vector<float*> means3 = {mean1, mean2};
-    // std::vector<float*> data3 = {data2, data3};
-    // std::vector<float> result3 = closest_distance(means3, data3);
-    // assert(result3.size() == 2);
-    // assert(result3[0] == 2.0);
-    // assert(result3[1] == 2.0);
-
+void runTests_closest_distance() {
+   
     const double epsilon = 0.000001;
 
     // Add more test cases as needed...
@@ -60,8 +35,6 @@ void runTests() {
     assert(result3[0] - 3.1622776601683795  <= epsilon ); // Distance between data3 and mean1
     assert(result3[1] - 3.1622776601683795 <= epsilon); // Distance between data4 and mean2
 
-    std::cout << "All tests passed!" << std::endl;
-
     // Test case 4: Multiple means and data points
 
      float mean3[10] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};   
@@ -81,11 +54,34 @@ void runTests() {
 
 }
 
+void test_closest_mean() {
+    // Cas de test 1: Un seul point avec un seul point de moyenne
+    float mean1[100] = {1.0}; // Moyenne simple
+    float point1[100] = {2.0}; // Point simple
+    std::vector<float*> means1 = {mean1};
+    uint32_t result1 = closest_mean(point1, means1);
+    
+    assert(result1 == 0); // La moyenne la plus proche devrait être la première (index 0)
+
+    // Cas de test 2: Plusieurs moyennes avec un seul point
+    float mean2[100] = {0.0}; // Première moyenne
+    float mean3[100] = {2.0}; // Deuxième moyenne
+    std::vector<float*> means2 = {mean2, mean3};
+    uint32_t result2 = closest_mean(point1, means2);
+    std::cout << result2 << std::endl;
+    assert(result2 == 1); // La moyenne la plus proche devrait être la première (index 0)
+
+    // Ajoutez plus de cas de test selon vos besoins...
+
+    std::cout << "Tous les tests sont passés avec succès!" << std::endl;
+}
+
 volatile int done = 0;
 
 int main() {
     // Your code here
-    runTests();
+    test_closest_mean();
+    
 
     return 0;
 }
